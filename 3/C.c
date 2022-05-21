35b062aaea099b4574ea77393e22a7d8917ec5f6
Alan_Chen <2304144391@qq.com>
2022-05-21 16:24:38 +0800

    Create C.c

---
+++ 3/C.c
@@ -0,0 +1,40 @@
+﻿#include<stdio.h>
+#include<math.h>
+int a = 6;
+void fun() {
+	int b, c, d;
+	for (b = 3; b <= a / 2; b += 2)
+	{
+		for (c = 2; c <= sqrt(b); c++)
+			if (b % c == 0)
+				break;
+		if (c > sqrt(b))
+			d = a - b;
+		else
+			break;
+
+		for (c = 2; c <= sqrt(d); c++)
+			if (d % c == 0)
+				break;
+		if (c > sqrt(d)) {
+			printf("%d=%d+%d\n", a, b, d);
+			a += 2;
+			break;
+		}
+	}
+	if (a == 98) {
+		printf("98=19+79\n");
+		a += 2;
+	}
+}
+int main() {
+	int max;
+	scanf("%d", &max);
+	while (1) {
+		fun();
+		if (a > max) {
+			break;
+		}
+	}
+	return 0;
+}
