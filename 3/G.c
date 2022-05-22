341dbcda4b3ce661002e8a69ee5bd536eb2bf17a
Alan_Chen <2304144391@qq.com>
2022-05-22 20:01:15 +0800

    1

---
+++ 3/G.c
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char str[100] = { 0 };
+	char rev[100] = { 0 };
+	int i, j = 0, k, m;
+	fgets(str, 100, stdin);
+	int len = strlen(str);
+	for (i = 0; i < len; i++) {
+		if (str[i] == '\n') {
+			str[i] = 0;
+		}
+	}
+	for (i = 0; i < len; i++) {
+
+		if (str[i] == ' ' || str[i] == '.' || str[i] == '!' || str[i] == '?') {
+			for (k = j - 1; k >= 0; k--) {
+				printf("%c", rev[k]);
+				if (k == 0) {
+					printf("%c", str[i]);
+					j = 0;
+					for (m = 0; m < 100; m++) {
+						rev[m] = 0;
+					}
+					break;
+				}
+			}
+		}
+		else
+			rev[j] = str[i];
+		j++;
+	}
+	return 0;
+}
---
+++ 3/G2.c
@@ -0,0 +1,52 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+void Reverse(char* left, char* right)
+{
+	while (left <= right)
+	{
+		char tmp = *left;
+		*left = *right;
+		*right = tmp;
+		left++;
+		right--;
+	}
+}
+
+int main()
+{
+	int i;
+	char str[100] = { 0 };
+	fgets(str, 100, stdin);
+	char mark = 0;
+	int len = strlen(str);
+	for (i = 0; i <= len; i++)
+		if (str[i] == '.' || str[i] == '!' || str[i] == '?') {
+			mark = str[i];
+			str[i] = '\n';
+		}
+	Reverse(str, str + len - 1);
+	char* start = str;
+	while (*start)
+	{
+		char* end = start;
+		while (*end != ' ' && *end)
+			end++;
+		Reverse(start, end - 1);
+		if (*end == ' ')
+			start = end + 1;
+		else
+			start = end;
+	}
+	for (i = 0; i < len; i++) {
+		if (str[i] == '\n')
+			str[i] = '\0';
+	}
+	for (i = len - 1; i >= 0; i--) {
+		if (str[i] != '\0')
+			printf("%c", str[i]);
+	}
+
+	printf("%c", mark);
+	return 0;
+}
