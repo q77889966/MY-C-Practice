6e67285e164f2d0b5541a8d6903815ab8ee053ac
Alan_Chen <2304144391@qq.com>
2022-05-28 21:44:40 +0800

    1

--- 4/My_C_Practice_4/My_C_Practice_4/D.c
+++ 4/My_C_Practice_4/My_C_Practice_4/D.c
@@ -4,8 +4,9 @@ typedef struct ch {
 	char c;
 	struct ch* next;
 }NODE;
-NODE* create(char* fp) {
-	NODE* head_1, * tail_1, * newp;
+NODE* create(char* fp, char* fp2, int n) {
+	int i;
+	NODE* head_1, * tail_1, * newp, * head_2, * tail_2, * head, * p;
 	head_1 = tail_1 = (NODE*)malloc(sizeof(NODE));
 	do {
 		newp = (NODE*)malloc(sizeof(NODE));
@@ -15,8 +16,24 @@ NODE* create(char* fp) {
 		fp++;
 	} while (*fp != '\n' && *fp != '\0');
 
+	head_2 = tail_2 = (NODE*)malloc(sizeof(NODE));
+
+	do {
+		newp = (NODE*)malloc(sizeof(NODE));
+		newp->c = *fp2;
+		tail_2->next = newp;
+		tail_2 = newp;
+		fp2++;
+	} while (*fp2 != '\n' && *fp2 != '\0');
+	head = head_1;
+	for (i = 0; i < n - 1; i++) {
+		head_1 = head_1->next;
+	}
+	p = head_1->next;
+	head_1->next = head_2->next;
+	tail_2->next = p;
 	tail_1->next = NULL;
-	return head_1;
+	return head;
 }
 void print(NODE* head) {
 	head = head->next;
@@ -27,9 +44,15 @@ void print(NODE* head) {
 }
 int main() {
 	char data[20] = { 0 };
+	char data2[20] = { 0 };
+	int n;
 	char* fp = data;
+	char* fp2 = data2;
 	fgets(data, 20, stdin);
-	NODE* head = create(fp);
+	fgets(data2, 20, stdin);
+	scanf("%d", &n);
+	NODE* head = create(fp, fp2, n);
+
 	print(head);
 	return 0;
 }
---
+++ 4/My_C_Practice_4/My_C_Practice_4/E.c
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+int main() {
+	int a[100] = { 0 };
+	int i;
+	FILE* fp;
+	fp = fopen("rand.txt", "r");
+
+	a[0] = fgetc(fp);
+
+	fclose(fp);
+	return 0;
+}
