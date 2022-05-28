8e86e4f4419c7dff70da3e23877664c7bd0928fa
Alan_Chen <2304144391@qq.com>
2022-05-28 19:45:28 +0800

    1

--- 4/My_C_Practice_4/My_C_Practice_4/C.c
+++ 4/My_C_Practice_4/My_C_Practice_4/C.c
@@ -28,16 +28,19 @@ void print(NODE* head) {
 int main() {
 	char data[20] = { 0 };
 	char* fp = data;
-	char c_1, c_2, tmp;
+	char c_1, c_2;
 	int n, i;
 	fgets(data, 20, stdin);
 	NODE* head_1 = create(fp);
 	NODE* head_2 = head_1;
 	scanf("%d", &n);
-	for (i = 0; i <= n; i++) {
+	for (i = 0; i < n; i++) {
 		head_1 = head_1->next;
 	}
-
+	c_1 = head_1->c;
+	c_2 = head_1->next->c;
+	head_1->c = c_2;
+	head_1->next->c = c_1;
 	print(head_2);
 	return 0;
 }
