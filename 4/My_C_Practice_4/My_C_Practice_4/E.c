54ad3c170145e99a679e557aa6b7937f19b785bf
Alan_Chen <2304144391@qq.com>
2022-05-29 10:23:48 +0800

    1

--- 4/My_C_Practice_4/My_C_Practice_4/E.c
+++ 4/My_C_Practice_4/My_C_Practice_4/E.c
@@ -2,12 +2,37 @@
 #include<stdlib.h>
 int main() {
 	int a[100] = { 0 };
-	int i;
+	int i, tmp, max = 0, j;
+	char c;
 	FILE* fp;
 	fp = fopen("rand.txt", "r");
+	i = 0;
+	while (1) {
+		while (1) {
+			fscanf(fp, "%d", &a[i]);
+			i++;
+			c = fgetc(fp);
+			if (c == '\n' || c == EOF)
+			{
+				break;
+			}
 
-	a[0] = fgetc(fp);
-
+		}
+		if (c == EOF)
+		{
+			break;
+		}
+	}
 	fclose(fp);
+	for (j = 0; j < 100; j++)
+		for (i = 0; i < 99; i++) {
+			if (a[i] < a[i + 1]) {
+				tmp = a[i];
+				a[i] = a[i + 1];
+				a[i + 1] = tmp;
+			}
+		}
+	printf("%d", a[9]);
+
 	return 0;
 }
